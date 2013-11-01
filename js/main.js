eaabfb96603b8290f5cc16bcaef13376853f33da
Julien Castelain <jcastelain@gmail.com>
2013-11-01 18:44:39 +0100

    Adding example

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-'use strict'; 
+(function () {
+  'use strict'; 
+
+  var game, img;
+
+  function preload() {
+    game.load.image('example', 'assets/example.png');
+  }
+
+  function create() {
+    img = game.add.sprite(400 - (128 / 2), 300 - (128 / 2), 'example');
+    img.anchor = new Phaser.Point(0.5, 0.5);
+  }
+
+  function update() {
+    img.rotation += 0.05;
+  }
+
+  game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
+    preload: preload,
+    create: create,
+    update: update
+  });
+}());
 
