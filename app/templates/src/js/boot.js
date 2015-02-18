59041efb17e94d00d031010160ffed5597f791f9
Glenn Geenen <glenn@geenentijd.be>
2015-02-18 16:35:47 +0100

    fix scale + center loader

--- app/templates/src/js/boot.js
+++ app/templates/src/js/boot.js
@@ -15,12 +15,12 @@
       if (this.game.device.desktop) {
         this.game.scale.pageAlignHorizontally = true;
       } else {
-        this.game.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.scale.minWidth =  480;
         this.game.scale.minHeight = 260;
         this.game.scale.maxWidth = 640;
         this.game.scale.maxHeight = 480;
-        this.game.scale.forceLandscape = true;
+        this.game.scale.forceOrientation(true);
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.setScreenSize(true);
       }
