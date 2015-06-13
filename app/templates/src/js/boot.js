dcb85d36d0adea329579e6bbd0cc9beb17135af4
Julien C <jcastelain@gmail.com>
2015-06-13 12:17:13 +0200

    Restore Boot config

--- app/templates/src/js/boot.js
+++ app/templates/src/js/boot.js
@@ -10,8 +10,20 @@
 
     create: function () {
       // configure game
+      this.game.input.maxPointers = 1;
 
-      // kick off preloader
+      if (this.game.device.desktop) {
+        this.game.scale.pageAlignHorizontally = true;
+      } else {
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.game.scale.minWidth =  480;
+        this.game.scale.minHeight = 260;
+        this.game.scale.maxWidth = 640;
+        this.game.scale.maxHeight = 480;
+        this.game.scale.forceOrientation(true);
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.setScreenSize(true);
+      }
       this.game.state.start('preloader');
     }
   };
