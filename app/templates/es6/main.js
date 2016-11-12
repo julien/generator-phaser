ccdd0584044c4a449a7657c61c079a95cf6f946a
Evan Cobb <cobb.evan@gmail.com>
2016-11-12 14:50:05 -0600

    removed single space at start of iterated template blocks
    because I get tired of manually killing those when I use this generator =)

--- app/templates/es6/main.js
+++ app/templates/es6/main.js
@@ -3,7 +3,7 @@ for(var i=0;i<gameStates.length;i++){
   var state = gameStates[i]
   var up_state = state.charAt(0).toUpperCase() + state.slice(1);
 
-%> import <%= up_state %> from './states/<%= state %>';
+%>import <%= up_state %> from './states/<%= state %>';
 <% } %>
 
 const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '<%= projectName %>-game');
@@ -13,6 +13,6 @@ for(var i=0;i<gameStates.length;i++){
   var state = gameStates[i];
   var up_state = state.charAt(0).toUpperCase() + state.slice(1);
 
-%> game.state.add('<%= state %>', new <%= up_state %>());
+%>game.state.add('<%= state %>', new <%= up_state %>());
 <% } %>
 game.state.start('boot');
