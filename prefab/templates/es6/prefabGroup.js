90c0151d688746cac94ab10858ee409643d96e38
Evan Cobb <cobb.evan@gmail.com>
2016-11-12 22:38:28 -0600

    Removing non-existent overrides from group, sprite, and tileSprite templates

--- prefab/templates/es6/prefabGroup.js
+++ prefab/templates/es6/prefabGroup.js
@@ -7,36 +7,6 @@ class <%= up_prefab %> extends Phaser.Group {
     super(game, parent);
   }
 
-  //Load operations (uses Loader), method called first
-  preload() {
-
-  }
-
-  //Setup code, method called after preload
-  create() {
-
-  }
-
-  //Code ran on each frame of game
-  update() {
-
-  }
-
-  //Called when game is paused
-  paused() {
-
-  }
-
-  //You're able to do any final post-processing style effects here.
-  render() {
-
-  }
-
-  //Called when switching to a new state
-  shutdown() {
-
-  }
-
 }
 
 export default <%= up_prefab %>;
