3cc577ba8c364a25fca84590ff07e13248c2e1b9
Evan Cobb <cobb.evan@gmail.com>
2017-01-17 13:26:04 -0600

    add basic editorconfig and reformat code

--- shader/index.js
+++ shader/index.js
@@ -6,45 +6,42 @@ var path = require('path');
 
 
 var ShaderGenerator = generators.Base.extend({
-  init: function () {
+  init: function() {
     this.log('Creating a new Shader.');
     this.projectName = this.config.get("projectName");
   },
 
-  askFor: function () {
-      var done = this.async();
-      var prompts = [
-          {
-              type: 'input',
-              name: 'shaderName',
-              message: 'What\'s the name of your new shader?',
-              filter: function(input){return path.basename(input, '.frag'); }, //remove the .frag file extension if it is there
-              validate: function(input) {
-                 if(input===""){
-                   return "Shader name cannot be empty";
-                 }/*
-                 else if(/[\^\&\'\@\{\}\[\]\,\$\=\!\#\(\)\.\%\+\~\?\s\ ]+$/.test(input)){ //test for illegal characters in filename
-                   return "Prefab name has invalid characters in file name"
-                 }*/
-                 else{
-                   return true;
-                 }
-              }
-            }
-      ];
-      this.prompt(prompts, function (props) {
-        //set global vars to prompt vars
-        this.shaderName = props.shaderName;
-        done();
-      }.bind(this));
+  askFor: function() {
+    var done = this.async();
+    var prompts = [{
+      type: 'input',
+      name: 'shaderName',
+      message: 'What\'s the name of your new shader?',
+      filter: function(input) {
+        return path.basename(input, '.frag');
+      }, //remove the .frag file extension if it is there
+      validate: function(input) {
+        if (input === "") {
+          return "Shader name cannot be empty";
+        }
+        else {
+          return true;
+        }
+      }
+    }];
+    this.prompt(prompts, function(props) {
+      //set global vars to prompt vars
+      this.shaderName = props.shaderName;
+      done();
+    }.bind(this));
   },
 
-  projectfiles: function () {
-      this.fs.copyTpl(
-        this.templatePath(path.join('shader.frag')),
-        this.destinationPath(path.join('src','shaders',this.shaderName+'.frag')),
-        this
-      );
+  projectfiles: function() {
+    this.fs.copyTpl(
+      this.templatePath(path.join('shader.frag')),
+      this.destinationPath(path.join('src', 'shaders', this.shaderName + '.frag')),
+      this
+    );
   }
 });
 
--- shader/templates/shader.frag
+++ shader/templates/shader.frag
@@ -1,11 +1,11 @@
 precision mediump float;
 
-uniform float     time;
-uniform vec2      resolution;
-uniform vec2      mouse;
+uniform float time;
+uniform vec2 resolution;
+uniform vec2 mouse;
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
 
 void main( void ) {
-    // Shader code goes here
+
 }
